108aff135150075b100027f0c5e74655a291a3b1
jmarkman <jmarkman446@gmail.com>
2018-03-07 23:55:11 -0500

    created recyclerview item, added recyclerview to gradle

---
+++ app/src/main/java/com/learningprojects/jmarkman/books/BooksAdapter.java
@@ -0,0 +1,15 @@
+package com.learningprojects.jmarkman.books;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by jmarkman on 3/7/2018.
+ */
+
+public class BooksAdapter
+{
+    public class BookViewHolder extends RecyclerView.ViewHolder
+    {
+
+    }
+}
